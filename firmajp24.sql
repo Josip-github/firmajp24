adbdf4646463c4fe4f809feadb58c881c3f57022
Josip Cota <josipcota1@gmail.com>
2021-05-12 17:58:32 +0200

    Kreiranje tablica

--- firmajp24.sql
+++ firmajp24.sql
@@ -1,2 +1,27 @@
 drop database if exists firmajp24;
 CREATE database firmajp24;
+use firmajp24;
+
+CREATE TABLE projekt(
+ 	sifra int NOT NULL primary KEY auto_increment,
+ 	naziv varchar(50),
+ 	cijena decimal(18,2)
+);
+
+CREATE TABLE programer(
+	sifra int NOT NULL primary KEY auto_increment,
+	ime varchar(50),
+	prezime varchar(50),
+	datumrodjenja datetime,
+	placa int
+	
+);
+
+CREATE TABLE sudjeluje(
+	projekt int,
+	programer int,
+	datumpocetka datetime,
+	datumkraja datetime
+);
+
+
